d2f0c300e3f1bb3417f775e3fd00b215de6d8d61
Paul Zerr <zerr.paul@gmail.com>
2026-02-18 14:26:05 +0100

    fixed ppt pdf generator

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 install_requires = [
-    "Scipy==1.14.1",
-    "numpy==2.1.2",
+    "scipy>=1.17.0,<1.18",
+    "numpy>=2.3.4,<2.4",
     "mne==1.8.0",
     "Flask==3.0.3",
     "peewee==3.17.6",
@@ -11,7 +11,7 @@ install_requires = [
     "edfio==0.4.3",
     "joblib==1.4.2",
     "pandas==2.2.3",
-    "numba==0.61.0",
+    "numba>=0.63.1,<0.64",
     "antropy==0.1.9",
     "lightgbm==4.6.0",
     "psg_utils",
